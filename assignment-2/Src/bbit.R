7db5d01f196f85d5566f2c5b0331bea584330544
Laxmipriyanka98 <126497356+Laxmipriyanka98@users.noreply.github.com>
2023-04-13 19:27:04 -0600

    Add files via upload

---
+++ assignment-2/Src/bbit.R
@@ -0,0 +1,7 @@
+# to determine the 98th percentile of the population and the sample's BMI
+BMI_98_sample <- quantile(sample_data$BMI, probs=0.98)
+BMI_98_pop <- quantile(diabetes$BMI, probs=0.98)
+
+# To compare BMI distributions, build a box plot.
+boxplot(diabetes$BMI, sample_data$BMI, names=c("Population", "Sample"), 
+        main="Comparing BMI Distributions", ylab="BMI",col="yellow")
